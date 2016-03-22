989b45463f6c5a06ee2fd0a3ac774b469470c966
Wade Armstrong <wade@wadearmstrong.com>
2016-03-21 20:06:33 -0700

    IN PROGRESS - testing for minty.splitfile

--- test/backend.js
+++ test/backend.js
@@ -1,10 +1,21 @@
 const expect = require('expect');
 const utils = require('../lib/utils.js');
 const path = require('path');
+const mediumFilePre = "var one = 'Greetings';\nvar two = 'Programs';\n\nvar three = 'Positive and negative, huh? You are a Bit.';";
+const mediumFilePost = [ 'var one = \'Greetings\';',
+  'var two = \'Programs\';',
+  '',
+  'var three = \'Positive and negative, huh? You are a Bit.\';' ]
 
-describe('Backend', () => {
-  it('should find files', () => {
-    .expect(utils.read(path.join(__dirname, 'dummyfiles/small.js')))
-      .toEqual("var one = 'hello';");
+describe('Backend', function() {
+  describe('#splitFile', function() {
+    it('should split files given a filename', function() {
+      expect(utils.splitFile(mediumFilePre), 'medium.js')
+      .toEqual(mediumFilePost);
+    });
+    it('should split files without a filename', function() {
+      expect(utils.splitFile(mediumFilePre))
+      .toEqual("['var one = \'Greetings\';', 'var two = \'Programs\';', '', 'var three = \'Positive and negative, huh? You are a Bit.\';']");
+    });
   });
 });
