ff45aa4a295c1eb9afa60af130c61633879f4eeb
Wade Armstrong <wade@wadearmstrong.com>
2016-03-22 12:33:23 -0700

    test splitFile; slight refactor splitFile for testability

--- test/backend.js
+++ test/backend.js
@@ -1,6 +1,44 @@
 const utils = require('../lib/utils.js');
 const expect = require('expect');
 
+const multiLineIn = [
+    'var one = {',
+    '  a: 1,',
+    '  b: 2',
+    '};',
+    '',
+    'var two = [',
+    '  "a",', '  "b"',
+    ']',
+    '',
+  ];
+  const multiLineOut = [
+    'var one = {  a: 1,  b: 2};',
+    '\n',
+    '\n',
+    '\n',
+    '\n',
+    'var two = [  "a",  "b"]',
+    '\n',
+    '\n',
+    '\n',
+    '\n',
+  ];
+
+const mediumIn = "var one = 'Greetings';\nvar two = 'Programs';\n\nvar three = 'Positive and negative, huh? You are a Bit.';";
+const mediumOut = ['var one = \'Greetings\';', 'var two = \'Programs\';', '', 'var three = \'Positive and negative, huh? You are a Bit.\';'];
+
+// Stub out the callbacks
+function returnFunc1(err, val1, val2) {
+  // Generic callback that can be used with Async's callback format;
+  return val1;
+}
+
+function returnFunc2(err, val1, val2) {
+  // Generic callback that can be used with Async's callback format;
+  return val2;
+}
+
 describe('Backend', () => {
   describe('#read', () => {
     it('read should exist', () => {
@@ -12,29 +50,21 @@ describe('Backend', () => {
       expect(utils.multiLineVarsCheck).toBeA(Function);
     });
     it('multiLineVarsCheck should convert multiliine variables into single line variables', () => {
-      expect(utils.multiLineVarsCheck([
-          'var one = {',
-          '  a: 1,',
-          '  b: 2',
-          '};',
-          '',
-          'var two = [',
-          '  "a",', '  "b"',
-          ']',
-          '',
-        ]))
-        .toEqual([
-          'var one = {  a: 1,  b: 2};',
-          '\n',
-          '\n',
-          '\n',
-          '\n',
-          'var two = [  "a",  "b"]',
-          '\n',
-          '\n',
-          '\n',
-          '\n',
-        ]);
+      expect(utils.multiLineVarsCheck(multiLineIn))
+        .toEqual(multiLineOut);
+    });
+  });
+  describe('#splitFile', () => {
+    it('splitFile should exist', () => {
+      expect(utils.splitFile).toBeA(Function);
+    });
+    it('should return an array of the function when passed a filename', () => {
+      expect(utils.splitFile(mediumIn, 'file', returnFunc1))
+        .toEqual(mediumOut);
+    });
+    it('should return an array of the function when NOT passed a filename', () =>{
+      expect(utils.splitFile(mediumIn, undefined, returnFunc1))
+        .toEqual(mediumOut);
     });
   });
   describe('#functionLineSwap', () => {
