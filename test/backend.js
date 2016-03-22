5af9bdc7dc643c97971f978e8338bec5b7d5857e
Ted Zilist <tzilist@gmail.com>
2016-03-21 20:06:47 -0700

    first iteration of tests completed

--- test/backend.js
+++ test/backend.js
@@ -1,10 +1,94 @@
-const expect = require('expect');
 const utils = require('../lib/utils.js');
-const path = require('path');
+const expect = require('expect');
 
 describe('Backend', () => {
-  it('should find files', () => {
-    .expect(utils.read(path.join(__dirname, 'dummyfiles/small.js')))
-      .toEqual("var one = 'hello';");
+  describe('#read', () => {
+    it('read should exist', () => {
+      expect(utils.read).toBeA(Function);
+    });
+  });
+  describe('#multiLineVarsCheck', () => {
+    it('multiLineVarsCheck should exist', () => {
+      expect(utils.multiLineVarsCheck).toBeA(Function);
+    });
+    it('multiLineVarsCheck should convert multiliine variables into single line variables', () => {
+      expect(utils.multiLineVarsCheck([
+          'var one = {',
+          '  a: 1,',
+          '  b: 2',
+          '};',
+          '',
+          'var two = [',
+          '  "a",', '  "b"',
+          ']',
+          '',
+        ]))
+        .toEqual([
+          'var one = {  a: 1,  b: 2};',
+          '\n',
+          '\n',
+          '\n',
+          '\n',
+          'var two = [  "a",  "b"]',
+          '\n',
+          '\n',
+          '\n',
+          '\n',
+        ]);
+    });
+  });
+  describe('#functionLineSwap', () => {
+    it('functionLineSwap should exist', () => {
+      expect(utils.functionLineSwap).toBeA(Function);
+    });
+    it('functionLineSwap should place check code injection before function call when function is called after declaration', () => {
+      expect(utils.functionLineSwap(['function hello(a, b, c) {\n',
+          'minty.variableLog(0, file, log, ["a","b","c"], a, b, c);\n',
+          '  console.log(a + b + c);\n',
+          'minty.variableLog(1, file, log, ["a","b","c"], a, b, c);\n',
+          '}\n',
+          '\n',
+          'hello(1, 2, 3);\n',
+          "minty.variableLog(4, file, log, [], '_mintyUndefined')",
+          '\n',
+        ]))
+        .toEqual(
+        [
+          'function hello(a, b, c) {\n',
+          'minty.variableLog(0, file, log, ["a","b","c"], a, b, c);\n',
+          '  console.log(a + b + c);\n',
+          'minty.variableLog(1, file, log, ["a","b","c"], a, b, c);\n',
+          '}\n',
+          '\n',
+          "minty.variableLog(4, file, log, [], '_mintyUndefined')",
+          'hello(1, 2, 3);\n',
+          '\n',
+        ]
+        );
+    });
+    it('functionLineSwap should place check code injection before function call when function is called before declaration', () => {
+      expect(utils.functionLineSwap([
+        'hello(1, 2, 3);\n',
+        'minty.variableLog(4, file, log, [], \'_mintyUndefined\')',
+        '\n',
+        'function hello(a, b, c) {\n',
+        'minty.variableLog(0, file, log, ["a","b","c"], a, b, c);\n',
+        '  console.log(a + b + c);\n',
+        'minty.variableLog(1, file, log, ["a","b","c"], a, b, c);\n',
+        '}\n',
+        '\n',
+      ]))
+        .toEqual([
+          'minty.variableLog(4, file, log, [], \'_mintyUndefined\')',
+          'hello(1, 2, 3);\n',
+          '\n',
+          'function hello(a, b, c) {\n',
+          'minty.variableLog(0, file, log, ["a","b","c"], a, b, c);\n',
+          '  console.log(a + b + c);\n',
+          'minty.variableLog(1, file, log, ["a","b","c"], a, b, c);\n',
+          '}\n',
+          '\n',
+        ]);
+    });
   });
 });
---
+++ test/dummyfiles/function.js
@@ -0,0 +1,5 @@
+hello(1, 2, 3);
+
+function hello(a, b, c) {
+  console.log(a + b + c);
+}
