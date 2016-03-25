525078d2e6df66381d7ed8500c76b5aa866f2099
Wade Armstrong <wade@wadearmstrong.com>
2016-03-25 12:01:09 -0700

    check that we are examining expected node types, and that all node types we are examining are tested

--- test/back-end-test.js
+++ test/back-end-test.js
@@ -1,6 +1,23 @@
 const parser = require('../lib/parser.js');
 const expect = require('expect');
 
+var checkedTypes = {
+  VariableDeclaration: false,
+  ForStatement: false,
+  ForInStatement: false,
+  ForOfStatement: false,
+  WhileStatement: false,
+  DoWhileStatement: false,
+  BreakStatement: false,
+  ReturnStatement: false,
+  YieldExpression: false,
+  FunctionDeclaration: false,
+  FunctionExpression: false,
+  ArrowFunctionExpression: false,
+  CallExpression: false,
+  IfStatement: false,
+  extra: 0, // this catches if we're examining ast types that we're not testing for
+};
 
 describe('Backend', () => {
   describe('#parser', () => {
@@ -9,6 +26,34 @@ describe('Backend', () => {
     });
   });
   describe('#parser\'s helper functions', () => {
+    describe('::types', () => {
+      it('types array should exist', () => {
+        expect(parser.tester.types).toBeAn(Array);
+      });
+      it('types array includes 14 types', () => {
+        expect(parser.tester.types.length).toEqual(14);
+      });
+      var theType;
+      parser.tester.types.forEach((type) => {
+        theType = type.type;
+        if (checkedTypes.hasOwnProperty(theType)) {
+          checkedTypes[theType] = true;
+        } else {
+          checkedTypes.extra++;
+        }
+      });
+      it('those 14 types being the 14 we plan to check for', () => {
+        var allKeys = true;
+        for (var keys in checkedTypes) {
+          allKeys = keys && allKeys;
+        }
+
+        expect(allKeys).toBe(true);
+      });
+      it('and all types we\'re checking for are also being tested', () => {
+        expect(checkedTypes.extra).toEqual(0);
+      });
+    });
     describe('#query', () => {
       it('query helper should exist', () => {
         expect(parser.tester.query).toBeA(Function);
@@ -32,6 +77,16 @@ describe('Backend', () => {
     describe('#parseFunction', () => {
       it('parseFunction helper should exist', () => {
         expect(parser.tester.parseFunction).toBeA(Function);
+      });
+    });
+    describe('#asyncTasks', () => {
+      it('asyncTasks builder helper should return an Array', () => {
+        expect(parser.tester.asyncTasks).toBeAn(Array);
+      });
+      it('asyncTasks array members should each be functions', () => {
+        parser.tester.asyncTasks.forEach((el) => {
+          expect(el).toBeA(Function);
+        });
       });
     });
   })
