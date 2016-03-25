099e3d87e523feb9ff6cea9f5c8f4ecffb6bf7fd
Wade Armstrong <wade@wadearmstrong.com>
2016-03-25 10:55:56 -0700

    checks for existence of all variables

--- test/back-end-test.js
+++ test/back-end-test.js
@@ -5,7 +5,34 @@ const expect = require('expect');
 describe('Backend', () => {
   describe('#parser', () => {
     it('parser should exist', () => {
-      expect(parser).toBeA(Function);
+      expect(parser.parser).toBeA(Function);
     });
   });
+  describe('#parser\'s helper functions', () => {
+    describe('#query', () => {
+      it('query helper should exist', () => {
+        expect(parser.tester.query).toBeA(Function);
+      });
+    });
+    describe('#functionParameterParse', () => {
+      it('functionParameterParse helper should exist', () => {
+        expect(parser.tester.functionParameterParse).toBeA(Function);
+      });
+    });
+    describe('#variableKindParse', () => {
+      it('variableKindParse helper should exist', () => {
+        expect(parser.tester.variableKindParse).toBeA(Function);
+      });
+    });
+    describe('#variableNameParse', () => {
+      it('variableNameParse helper should exist', () => {
+        expect(parser.tester.variableNameParse).toBeA(Function);
+      });
+    });
+    describe('#parseFunction', () => {
+      it('parseFunction helper should exist', () => {
+        expect(parser.tester.parseFunction).toBeA(Function);
+      });
+    });
+  })
 });
