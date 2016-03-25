1d8fe56503d1c61c4bc91cf9c6d229aa6eccdcb7
Ted Zilist <tzilist@gmail.com>
2016-03-25 10:12:15 -0700

    working on injector

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const parser = require('./lib/parser.js');
 const ruler = require('./lib/createLineRules.js');
 const inject = require('./lib/injector.js');
+const fs = require('fs');
 
 var minty = {};
 
 minty.file = function file(path) {
-  const parsed = parser(path);
+  const JSTEXT = fs.readFileSync(path);
+  const parsed = parser(JSTEXT);
   const rules = ruler(parsed);
-  console.log(JSON.stringify(rules));
+  const injected = inject(rules, JSTEXT);
 };
 
 minty.wrap = function wrap(func) {
