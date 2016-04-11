171b6f391310584c9b9b5342efebc10563bbbad9
angie88 <angieyeh24@gmail.com>
2016-04-10 20:47:01 -0700

    testing to see if ruler function is outputting the expected output

--- test/backend/test-createLR.js
+++ test/backend/test-createLR.js
@@ -1,11 +1,23 @@
-const createLR = require('./../../lib/createLineRules.js')
+'use strict';
+const createLR = require('./../../lib/createLineRules.js');
+const createLRFixture = require('./../fixtures/createLR-input.js');
+const lineActivityFixture = require('./../fixtures/lineActivityObj.js')
 const expect = require('expect');
 const test = require('tape');
+const sinon = require('sinon');
 
 test('ruler', (t) => {
-  t.plan(1);
+  t.plan(2);
+
+  let addScopeName = sinon.spy(createLR.addScopeName);
+  var lineActivityObj = createLR.ruler(createLRFixture);
+  // console.log('called once', addScopeName.callCount);
+
   t.equal(typeof createLR.ruler, 'function');
+  t.deepEqual(lineActivityObj, lineActivityFixture)
 });
+
+
 
 test('addLines', (t) => {
   t.plan(1);
