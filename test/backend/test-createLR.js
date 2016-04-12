4727d4bf578d922e7b79e7ab0df0dc48422c8041
angie88 <angieyeh24@gmail.com>
2016-04-11 17:26:24 -0700

    testing addLines function is manipulating the lineactivity object as expected when invoked

--- test/backend/test-createLR.js
+++ test/backend/test-createLR.js
@@ -1,6 +1,7 @@
 'use strict';
 const createLR = require('./../../lib/createLineRules.js');
-const createLRFixture = require('./../fixtures/createLR-input.js');
+const createLRInput = require('./../fixtures/createLR-input.js');
+const createLROutput = require('./../fixtures/createLR-output.js');
 const lineActivityFixture = require('./../fixtures/lineActivityObj.js')
 const expect = require('expect');
 const test = require('tape');
@@ -9,19 +10,23 @@ const sinon = require('sinon');
 test('ruler', (t) => {
   t.plan(2);
 
-  let addScopeName = sinon.spy(createLR.addScopeName);
-  var lineActivityObj = createLR.ruler(createLRFixture);
-  // console.log('called once', addScopeName.callCount);
+  let addScopeName = sinon.spy(createLR, 'addScopeName');
+  var lineActivityObj = createLR.ruler(createLRInput.parsed);
 
   t.equal(typeof createLR.ruler, 'function');
-  t.deepEqual(lineActivityObj, lineActivityFixture)
+  t.deepEqual(lineActivityObj, lineActivityFixture);
+  console.log('called once', addScopeName.callCount);
 });
 
 
 
 test('addLines', (t) => {
-  t.plan(1);
+  t.plan(2);
+  let lineActivity = {};
+  createLR.addLines('FunctionDeclaration', 'SCOPE', createLRInput.parseFunctionDec, lineActivity);
+  t.deepEqual(lineActivity, createLROutput.addLines)
   t.equal(typeof createLR.addLines, 'function');
+
 });
 
 test('ruler', (t) => {
